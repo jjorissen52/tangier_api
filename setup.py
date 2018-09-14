a659fba477a0b8aa65803c04742e122b087e7db6
jjorissen52 <jjorissen52@gmail.com>
2018-09-14 12:23:25 -0500

    pinned correct version of xmlmanip

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0.dev',
+    version='1.2.1.dev',
 
     description="A simple Python package to facilitate interactions with Tangier's API",
     long_description=open('README.rst').read(),
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['xmlmanip=1.1.8', 'requests==2.18.3', 'zeep==2.3.0', 'bs4', 'pandas', 'xlsxwriter', 'xlrd'],
+    install_requires=['xmlmanip==1.1.8.dev0', 'requests==2.18.3', 'zeep==2.3.0', 'bs4', 'pandas', 'xlsxwriter', 'xlrd'],
 
     python_requires='>=3.6',
 )
